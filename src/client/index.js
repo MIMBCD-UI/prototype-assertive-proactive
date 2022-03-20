a4bce5edab766add7d726e2dc446838ceaac7cff
JGMF99 <48356138+JGMF99@users.noreply.github.com>
2022-03-20 14:19:32 +0000

    [FIX] closes #8; Fix explanation when closing patient

--- src/client/index.js
+++ src/client/index.js
@@ -120,7 +120,9 @@ $.getJSON(studyListFile, function(data) {
             }
             // Close Assistant block
             $('.assistant_block').css({display:'none'});
+            $('.assistant_block_covariables').css({display:'none'});
             assistant_animation_out();
+            
 
           });
 
@@ -230,6 +232,10 @@ function assistant_animation_in() {
 // Assistant Avatar Animation
 function assistant_animation_out() {
   $(".assistant_block").delay(1).animate({
+    opacity: '0',
+    right: '-150px'
+  });
+  $(".assistant_block_covariables").delay(1).animate({
     opacity: '0',
     right: '-150px'
   });
